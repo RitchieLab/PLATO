2be756f8d17f038378d3f3594f23b2838d1928ce
jrw32@dce.psu.edu <jrw32@dce.psu.edu@9e62ecf6-770d-0410-a9d7-e62a33e3d092>
2014-04-22 14:46:25 +0000

    - Added extra logging of begin/end times for each step along the way

--- Process.cpp
+++ Process.cpp
@@ -1,6 +1,14 @@
 #include "Process.h"
 
+#include <boost/lexical_cast.hpp>
+#include <boost/date_time.hpp>
+
+#include "util/Logger.h"
+
 namespace po=boost::program_options;
+
+using PLATO::Utility::Logger;
+using std::string;
 
 namespace PLATO{
 
@@ -10,8 +18,12 @@ po::options_description& Process::addOptions(po::options_description& opts){
 }
 
 void Process::run(PLATO::Data::DataSet& ds){
+	Logger::log(boost::lexical_cast<string>(boost::posix_time::second_clock::local_time())
+			+ ": Starting " + getName());
 	process(ds);
 	PrintSummary();
+	Logger::log(boost::lexical_cast<string>(boost::posix_time::second_clock::local_time())
+			+ ": Finished " + getName());
 }
 
 }
