56d63e35ecef571e76c6e3d17c25cd5869482862
jrw32@dce.psu.edu <jrw32@dce.psu.edu@9e62ecf6-770d-0410-a9d7-e62a33e3d092>
2014-08-22 17:56:28 +0000

    Hopefully this will fix bug #449, checking in the right file, this time!

--- method_lib/util/CommandLineParser.h
+++ method_lib/util/CommandLineParser.h
@@ -148,208 +148,196 @@ custom_command_line_parser<charT>::run()
 	using namespace boost::program_options::command_line_style;
 	using std::vector;
 	using std::string;
-//	using namespace boost::program_options::detail::
 
-    // save the canonical prefixes which were used by this cmdline parser
-    //    eventually inside the parsed results
-    //    This will be handy to format recognisable options
-    //    for diagnostic messages if everything blows up much later on
-    parsed_options super_result(m_desc, detail::cmdline::get_canonical_option_prefix());
+	parsed_options super_result(m_desc);
 
     // The parsing is done by having a set of 'style parsers'
-    // and trying then in order. Each parser is passed a vector
-    // of unparsed tokens and can consume some of them (by
-    // removing elements on front) and return a vector of options.
-    //
-    // We try each style parser in turn, untill some input
-    // is consumed. The returned vector of option may contain the
-    // result of just syntactic parsing of token, say --foo will
-    // be parsed as option with name 'foo', and the style parser
-    // is not required to care if that option is defined, and how
-    // many tokens the value may take.
-    // So, after vector is returned, we validate them.
-    assert(m_desc);
+     // and trying then in order. Each parser is passed a vector
+     // of unparsed tokens and can consume some of them (by
+     // removing elements on front) and return a vector of options.
+     //
+     // We try each style parser in turn, untill some input
+     // is consumed. The returned vector of option may contain the
+     // result of just syntactic parsing of token, say --foo will
+     // be parsed as option with name 'foo', and the style parser
+     // is not required to care if that option is defined, and how
+     // many tokens the value may take.
+     // So, after vector is returned, we validate them.
+     assert(m_desc);
 
-    vector<style_parser> style_parsers;
+     vector<style_parser> style_parsers;
 
-    if (m_style_parser)
-        style_parsers.push_back(m_style_parser);
+     if (m_style_parser)
+         style_parsers.push_back(m_style_parser);
 
-    if (m_additional_parser)
-        style_parsers.push_back(
-            boost::bind(&cmdline::handle_additional_parser, this, _1));
+     if (m_additional_parser)
+         style_parsers.push_back(
+             boost::bind(&cmdline::handle_additional_parser, this, _1));
 
-    if (m_style & allow_long)
-        style_parsers.push_back(
-            boost::bind(&cmdline::parse_long_option, this, _1));
+     if (m_style & allow_long)
+         style_parsers.push_back(
+             boost::bind(&cmdline::parse_long_option, this, _1));
 
-    if ((m_style & allow_long_disguise))
-        style_parsers.push_back(
-            boost::bind(&cmdline::parse_disguised_long_option, this, _1));
+     if ((m_style & allow_long_disguise))
+         style_parsers.push_back(
+             boost::bind(&cmdline::parse_disguised_long_option, this, _1));
 
-    if ((m_style & allow_short) && (m_style & allow_dash_for_short))
-        style_parsers.push_back(
-            boost::bind(&cmdline::parse_short_option, this, _1));
+     if ((m_style & allow_short) && (m_style & allow_dash_for_short))
+         style_parsers.push_back(
+             boost::bind(&cmdline::parse_short_option, this, _1));
 
-    if ((m_style & allow_short) && (m_style & allow_slash_for_short))
-        style_parsers.push_back(boost::bind(&cmdline::parse_dos_option, this, _1));
+     if ((m_style & allow_short) && (m_style & allow_slash_for_short))
+         style_parsers.push_back(boost::bind(&cmdline::parse_dos_option, this, _1));
 
-    style_parsers.push_back(boost::bind(&cmdline::parse_terminator, this, _1));
+     style_parsers.push_back(boost::bind(&cmdline::parse_terminator, this, _1));
 
-    vector<option> result;
-    bool ok=true;
-    while(!args.empty() && ok)
-    {
-        ok = false;
-        for(unsigned i = 0; i < style_parsers.size(); ++i)
-        {
-            unsigned current_size = static_cast<unsigned>(args.size());
-            vector<option> next = style_parsers[i](args);
+     vector<option> result;
+     while(!args.empty())
+     {
+         bool ok = false;
+         for(unsigned i = 0; i < style_parsers.size(); ++i)
+         {
+             unsigned current_size = args.size();
+             vector<option> next = style_parsers[i](args);
 
-            // Check that option names
-            // are valid, and that all values are in place.
-            if (!next.empty())
-            {
-                vector<string> e;
-                for(unsigned k = 0; k < next.size()-1; ++k) {
-                    finish_option(next[k], e, style_parsers);
-                }
-                // For the last option, pass the unparsed tokens
-                // so that they can be added to next.back()'s values
-                // if appropriate.
-                finish_option(next.back(), args, style_parsers);
-                for (unsigned j = 0; j < next.size(); ++j)
-                    result.push_back(next[j]);
-            }
+             // Check that option names
+             // are valid, and that all values are in place.
+             if (!next.empty())
+             {
+                 vector<string> e;
+                 for(unsigned k = 0; k < next.size()-1; ++k) {
+                     finish_option(next[k], e, style_parsers);
+                 }
+                 // For the last option, pass the unparsed tokens
+                 // so that they can be added to next.back()'s values
+                 // if appropriate.
+                 finish_option(next.back(), args, style_parsers);
+                 for (unsigned j = 0; j < next.size(); ++j)
+                     result.push_back(next[j]);
+             }
 
-            if (args.size() != current_size) {
-                ok = true;
-                break;
-            }
-        }
+             if (args.size() != current_size) {
+                 ok = true;
+                 break;
+             }
+         }
 
-        if (!ok) {
-        	while(!args.empty()){
-	            option opt;
-    	        opt.value.push_back(args[0]);
-    	        opt.original_tokens.push_back(args[0]);
-    	        result.push_back(opt);
-    	        args.erase(args.begin());
-    	    }
-        }
-    }
+         if (!ok) {
+         	while(!args.empty()){
+ 	            option opt;
+     	        opt.value.push_back(args[0]);
+     	        opt.original_tokens.push_back(args[0]);
+     	        result.push_back(opt);
+     	        args.erase(args.begin());
+     	    }
+         }
+     }
 
-    /* If an key option is followed by a positional option,
-        can can consume more tokens (e.g. it's multitoken option),
+     /* If an key option is followed by a positional option,
+        can can consume more tokens (e.g. it's multitoke option),
         give those tokens to it.  */
-    vector<option> result2;
-    for (unsigned i = 0; i < result.size(); ++i)
-    {
-        result2.push_back(result[i]);
-        option& opt = result2.back();
+     vector<option> result2;
+     for (unsigned i = 0; i < result.size(); ++i)
+     {
+         result2.push_back(result[i]);
+         option& opt = result2.back();
 
-        if (opt.string_key.empty())
-            continue;
+         if (opt.string_key.empty())
+             continue;
 
-        const option_description* xd;
-        try
-        {
-            xd = m_desc->find_nothrow(opt.string_key,
-                                        is_style_active(allow_guessing),
-                                        is_style_active(long_case_insensitive),
-                                        is_style_active(short_case_insensitive));
-        }
-        catch(error_with_option_name& e)
-        {
-            // add context and rethrow
-            e.add_context(opt.string_key, opt.original_tokens[0], get_canonical_option_prefix());
-            throw;
-        }
+         const option_description* xd =
+             m_desc->find_nothrow(opt.string_key,
+                                     is_style_active(allow_guessing),
+                                     is_style_active(long_case_insensitive),
+                                     is_style_active(short_case_insensitive));
+         if (!xd)
+             continue;
 
-        if (!xd)
-            continue;
+         unsigned min_tokens = xd->semantic()->min_tokens();
+         unsigned max_tokens = xd->semantic()->max_tokens();
+         if (min_tokens < max_tokens && opt.value.size() < max_tokens)
+         {
+             // This option may grab some more tokens.
+             // We only allow to grab tokens that are not already
+             // recognized as key options.
 
-        unsigned min_tokens = xd->semantic()->min_tokens();
-        unsigned max_tokens = xd->semantic()->max_tokens();
-        if (min_tokens < max_tokens && opt.value.size() < max_tokens)
-        {
-            // This option may grab some more tokens.
-            // We only allow to grab tokens that are not already
-            // recognized as key options.
+             int can_take_more = max_tokens - opt.value.size();
+             unsigned j = i+1;
+             for (; can_take_more && j < result.size(); --can_take_more, ++j)
+             {
+                 option& opt2 = result[j];
+                 if (!opt2.string_key.empty())
+                     break;
 
-            int can_take_more = max_tokens - static_cast<int>(opt.value.size());
-            unsigned j = i+1;
-            for (; can_take_more && j < result.size(); --can_take_more, ++j)
-            {
-                option& opt2 = result[j];
-                if (!opt2.string_key.empty())
-                    break;
+                 if (opt2.position_key == INT_MAX)
+                 {
+                     // We use INT_MAX to mark positional options that
+                     // were found after the '--' terminator and therefore
+                     // should stay positional forever.
+                     break;
+                 }
 
-                if (opt2.position_key == INT_MAX)
-                {
-                    // We use INT_MAX to mark positional options that
-                    // were found after the '--' terminator and therefore
-                    // should stay positional forever.
-                    break;
-                }
+                 assert(opt2.value.size() == 1);
 
-                assert(opt2.value.size() == 1);
+                 opt.value.push_back(opt2.value[0]);
 
-                opt.value.push_back(opt2.value[0]);
+                 assert(opt2.original_tokens.size() == 1);
 
-                assert(opt2.original_tokens.size() == 1);
-
-                opt.original_tokens.push_back(opt2.original_tokens[0]);
-            }
-            i = j-1;
-        }
-    }
-    result.swap(result2);
+                 opt.original_tokens.push_back(opt2.original_tokens[0]);
+             }
+             i = j-1;
+         }
+     }
+     result.swap(result2);
 
 
-    // Assign position keys to positional options.
-    int position_key = 0;
-    for(unsigned i = 0; i < result.size(); ++i) {
-        if (result[i].string_key.empty())
-            result[i].position_key = position_key++;
-    }
+     // Assign position keys to positional options.
+     int position_key = 0;
+     for(unsigned i = 0; i < result.size(); ++i) {
+         if (result[i].string_key.empty())
+             result[i].position_key = position_key++;
+     }
 
-    if (m_positional)
-    {
-        unsigned position = 0;
-        for (unsigned i = 0; i < result.size(); ++i) {
-            option& opt = result[i];
-            if (opt.position_key != -1) {
-                if (position >= m_positional->max_total_count())
-                {
-                    boost::throw_exception(too_many_positional_options_error());
-                }
-                opt.string_key = m_positional->name_for_position(position);
-                ++position;
-            }
-        }
-    }
+     if (m_positional)
+     {
+         unsigned position = 0;
+         for (unsigned i = 0; i < result.size(); ++i) {
+             option& opt = result[i];
+             if (opt.position_key != -1) {
+                 if (position >= m_positional->max_total_count())
+                 {
+                     boost::throw_exception(too_many_positional_options_error());
+                 }
+                 opt.string_key = m_positional->name_for_position(position);
+                 ++position;
+             }
+         }
+     }
 
-    // set case sensitive flag
-    for (unsigned i = 0; i < result.size(); ++i) {
-        if (result[i].string_key.size() > 2 ||
-                    (result[i].string_key.size() > 1 && result[i].string_key[0] != '-'))
-        {
-            // it is a long option
-            result[i].case_insensitive = is_style_active(long_case_insensitive);
-        }
-        else
-        {
-            // it is a short option
-            result[i].case_insensitive = is_style_active(short_case_insensitive);
-        }
-    }
+     // set case sensitive flag
+     for (unsigned i = 0; i < result.size(); ++i) {
+         if (result[i].string_key.size() > 2 ||
+                     (result[i].string_key.size() > 1 && result[i].string_key[0] != '-'))
+         {
+             // it is a long option
+             result[i].case_insensitive = is_style_active(long_case_insensitive);
+         }
+         else
+         {
+             // it is a short option
+             result[i].case_insensitive = is_style_active(short_case_insensitive);
+         }
+     }
 
-    super_result.options = result;
+     super_result.options = result;
+
+     return super_result;
+
+
 
     // Presense of parsed_options -> wparsed_options conversion
     // does the trick.
-    return basic_parsed_options<charT>(super_result);
+    //return basic_parsed_options<charT>(super_result);
 }
 
 }
